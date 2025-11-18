f7ff06d6bc6164d4d0f2bc60169666ebcb688ea9
Fatom Al-Ghanmi <fatom.alghanmi@triosstudent.com>
2025-11-18 10:42:34 -0500

    arrays updated

--- src/main/java/org/example/GeographyQuiz.kt
+++ src/main/java/org/example/GeographyQuiz.kt
@@ -1,23 +1,57 @@
 package org.example
 
+import kotlin.random.Random
+
 fun main() {
 
-    val provinces = arrayOf("Alberta", "British Columbia", "Manitoba", "New Brunswick",
+    val provinces = arrayOf(
+        "Alberta", "British Columbia", "Manitoba", "New Brunswick",
         "Newfoundland & Labrador", "Northwest Territories",
-        "Nova Scotia", "Nunavut", "Ontario", "Prince Edward Island", "Quebec", "Saskatchewan", "Yukon")
+        "Nova Scotia", "Nunavut", "Ontario", "Prince Edward Island", "Quebec", "Saskatchewan", "Yukon"
+    )
 
-
-    val capitals = arrayOf("Edmonton", "Victoria", "Winnipeg", "Fredericton",
+    val capitals = arrayOf(
+        "Edmonton", "Victoria", "Winnipeg", "Fredericton",
         "St. John's", "Yellowknife", "Halifax", "Iqaluit", "Toronto",
-        "Charlottetown", "Quebec City", "Regina", "Whitehorse")
+        "Charlottetown", "Quebec City", "Regina", "Whitehorse"
+    )
 
-    var duplicates =arrayOf(false, false, false, false,
+    var duplicates = arrayOf(
         false, false, false, false,
         false, false, false, false,
-        false)
+        false, false, false, false,
+        false
+    )
 
-    var answer = ""
+    var answer: String
     var correct = 0
 
     println("Welcome to the Provinces Geography Quiz - Good Luck!")
+
+    var question = 0
+
+    for (count in 0..12) {
+
+        // pick unused question index
+        do {
+            question = Random.nextInt(0, 13)
+        } while (duplicates[question])
+
+        duplicates[question] = true
+
+        println("What is the capital of ${provinces[question]}?")
+        print("Your answer: ")
+
+        answer = readln()
+
+        if (answer.equals(capitals[question], ignoreCase = true)) {
+            println("Congratulations, you are correct!")
+            correct += 1
+        } else {
+            println("Sorry, you are incorrect. The correct answer is ${capitals[question]}.")
+        }
+        println() // blank line for readability
+    }
+
+    println("You got $correct question(s) correct.")
 }
